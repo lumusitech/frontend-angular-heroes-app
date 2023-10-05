a220857f56bdc0188f3627283e6e3bd4282819ad
lumusitech <lumusitech@gmail.com>
2023-10-05 12:31:23 -0300

    feat: add CRUD to heroes service

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { environments } from 'environments/environments';
 import { Hero } from '../interfaces/hero.interface';
@@ -25,5 +25,22 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes?q=${query}&_limit=5`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseURL}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseURL}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseURL}/heroes/${id}`).pipe(
+      catchError((error) => of(false)),
+      map((resp) => true)
+    );
   }
 }
